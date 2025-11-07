2a680c3d557e77a2c9b669058429e965a2d9f106
Raaju <mrahmanraaju00@gmail.com>
2025-11-07 12:58:46 +0600

    top bar

--- app/src/main/java/com/autoappsheduler/MainActivity.kt
+++ app/src/main/java/com/autoappsheduler/MainActivity.kt
@@ -19,8 +19,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
@@ -32,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -46,12 +49,18 @@ import java.util.Calendar
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppSchedulerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+                    }
+                ) { innerPadding ->
                     MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
--- app/src/main/java/com/autoappsheduler/model/AppInfo.kt
+++ app/src/main/java/com/autoappsheduler/model/AppInfo.kt
@@ -1,7 +1,9 @@
 package com.autoappsheduler.model
 
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.Stable
 
+@Stable
 data class AppInfo(
     val appName: String,
     val packageName: String,
